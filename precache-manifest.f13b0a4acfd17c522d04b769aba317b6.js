f2ecdbba17938df2ebeeffc28e617d8e2a43f0dd
Elvis Kerebi <kerebipreye@gmail.com>
2020-08-19 14:00:56 -0800

    Updates

---
+++ precache-manifest.f13b0a4acfd17c522d04b769aba317b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43feba4dcaa4453d5f05232da1344ee7",
+    "url": "/personal-work/index.html"
+  },
+  {
+    "revision": "1809b745ea611f03aafd",
+    "url": "/personal-work/static/css/main.b631ec9a.chunk.css"
+  },
+  {
+    "revision": "0b7b96c99ca0e8db23cc",
+    "url": "/personal-work/static/js/2.3bf8037e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal-work/static/js/2.3bf8037e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1809b745ea611f03aafd",
+    "url": "/personal-work/static/js/main.2a77ee99.chunk.js"
+  },
+  {
+    "revision": "55cbe8139c4319955b9f",
+    "url": "/personal-work/static/js/runtime-main.ae034634.js"
+  },
+  {
+    "revision": "b540a06bae3fcea1221cd7f29d5dc869",
+    "url": "/personal-work/static/media/header-image.b540a06b.jpg"
+  }
+]);
